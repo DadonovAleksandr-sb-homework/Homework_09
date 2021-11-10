7db15cc6139e5c8f9ca89bb5b51d783dff92c0a5
DadonovAD <DadonovAD@tvv.transneft.ru>
2021-11-10 17:22:54 +0300

    Базовый функционал

--- Homework_09/Program.cs
+++ Homework_09/Program.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.IO;
-using System.Net;
-using Telegram.Bot;
-using Telegram.Bot.Args;
-using Telegram.Bot.Types.Enums;
 
 
 namespace Homework_09
 {
     class Program
     {
-        private static TelegramBotClient bot;
         static void Main(string[] args)
         {
             // Бот обладает следующим набором функций:
@@ -30,47 +24,25 @@ namespace Homework_09
             //     Бот реагирует на команду /start.
             //     Бот позволяет сохранять на диск изображения, аудио- и другие файлы.
             //     С помощью произвольной команды можно просмотреть список сохранённых файлов и скачать любой из них.
-            
-            
-            string token = "1734825343:AAEHjIFX2yF4bZ3ZWLZJRb8WBl5TTbDWN6U";
 
-            bot = new TelegramBotClient(token);
+            string token = "1734825343:AAEHjIFX2yF4bZ3ZWLZJRb8WBl5TTbDWN6U";        // TODO: вынести токен в файл
 
-            bot.OnMessage += MessageListener;
-            bot.StartReceiving();
-            
+            try
+            {
+                var bot = new MyTelegramBot(token);
+                if(bot.Start())
+                    Console.WriteLine($"Запуск бота {bot.Name}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+                
             Console.ReadLine();
         }
 
-        private static void MessageListener(object? sender, MessageEventArgs e)
-        {
-            string text =
-                $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
-            Console.WriteLine($"{text} TypeMessage: {e.Message.Type.ToString()}");
+        
 
-            if (e.Message.Type == MessageType.Document)
-            {
-                Console.WriteLine(e.Message.Document.FileId);
-                Console.WriteLine(e.Message.Document.FileName);
-                Console.WriteLine(e.Message.Document.FileSize);
-
-                Download(e.Message.Document.FileId, e.Message.Document.FileName);
-            }
-
-            if (e.Message.Text == null) return;
-            var messageText = e.Message.Text;
-
-            bot.SendTextMessageAsync(e.Message.Chat.Id, $"{messageText}");
-        }
-
-        private static async void Download(string fileId, string path)
-        {
-            var file = await bot.GetFileAsync(fileId);
-            FileStream fs = new FileStream("_" + path, FileMode.Create);
-            await bot.DownloadFileAsync(file.FilePath, fs);
-            fs.Close();
-            
-            fs.Dispose();
-        }
     }
 }
